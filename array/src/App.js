94c3035b72dd34e5e64ca71ef2416f492e4c72c4
ah-ryeong <aryeong22@hanmail.net>
2021-10-11 19:35:57 +0900

    배열에 항목 추가하기

--- array/src/App.js
+++ array/src/App.js
@@ -1,11 +1,76 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import CreateUser from './CreateUser';
 import UserList from './UserList'
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+
+  // inputs 에서 username 이랑 email 추출
+  const {username, email} = inputs;
+  
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs( {
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {
+        id : 1,
+        username : 'winter',
+        email : 'winter@example.com'
+    },
+    {
+        id : 2,
+        username : 'tester',
+        email : 'tester@example.com'
+    },
+    {
+        id : 3,
+        username : 'tester2',
+        email : 'tester2@example.com'
+    }
+]);
+
+// 배열에서도 users에다가 값을 추가한다해서 push, splice, sort 같은 함수 사용 X
+// 꼭 사용해야 할 경우에는 배열을 복사하고(... 붙여서 하는거) 사용해야함
+const nextId = useRef(4);
+
+const onCreate = () => {
+  // 새로운 유저값 만들기
+  const user = {
+    id : nextId.current,
+    username,
+    email,
+  };
+
+  setUsers(users.concat(user));
+
+  // 클릭할 때, 인풋에 있는 값 지우기
+  setInputs({
+    username : '',
+    email : '',
+  });
+
+  nextId.current += 1;
+}
+
+
   return (
-    <div>
-      <UserList />
-    </div>
+    <>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
   );
 }
 
--- array/src/UserList.js
+++ array/src/UserList.js
@@ -12,35 +12,12 @@ function User({user}) {
 }
 
 // 배열 렌더링 하기 
-function UserList() {
-    const users =[
-        {
-            id : 1,
-            username : 'winter',
-            email : 'winter@example.com'
-        },
-        {
-            id : 2,
-            username : 'tester',
-            email : 'tester@example.com'
-        },
-        {
-            id : 3,
-            username : 'tester2',
-            email : 'tester2@example.com'
-        },
-    ];
-
+function UserList({users}) {
     return(
         <div>
             {
                 users.map(
-                    // key값을 지정해줘야함(고유값)
-                    user => {<User user ={user} key={user.id}/>}
-                    // 만약 지정할 고유값이 없는 경우, map 함수의 index를 사용해도 됨
-                    // 왠만하면 key에 index를 넣는건 피하는게 좋다. 정확한 key 값이 있어야 어떤 element를 가르키고 있는지 확실히 알 수 있기 때문에,
-                    // 배열에 값을 제거하거나 추가할 때 효율적으로 작업할 수 있다.
-                    // (user, index) => {<User user ={user} key={index}/>}
+                    user => (<User user ={user} key={user.id}/>)
                 )
             }
         </div>
